328f9a0e6d41df4baeaccd3e5917db3932e484b9
Joakim Husmo <joakimhusmo@outlook.com>
2025-05-02 13:52:18 +0200

    add expression Loop to script.js

--- script.js
+++ script.js
@@ -31,10 +31,32 @@ const display = document.getElementById('displayWindow');
   // Empty arrays for numbers and operators: As inputs are looped through, they're
   // pushed to the appropriate array.
   // currentNumber, a string that stores current number being built, e.g. '12' as 
-  // input '1' and '2' is clicked. Onces the operator is clicked, currentNumber is pushed to
+  // input '1' and '2' is clicked. Once the operator is clicked, currentNumber is pushed to
   // numbers array, and the operator is pushed to the operators array.
   function calculateExpression() {
     let numbers = [];
     let operators = [];
     let currentNumber = '';
+
+    // Loop goes through math expression, character by character, where 'i' is the
+    // index-position, and 'expression.length' total number of characters. 
+    // 'currChar' gets the character at the current index-position.
+    // If the character is a number, it's added to currentNumber.
+    // Ones the operator is clicked, currentNumber is pushed to numbers array, 
+    // after being converted to a number using Number(). 
+    // The operator is pushed to the operators array, and currentNumber is reset to ''.
+    for (let i = 0; i < expression.length; i++) {
+      const currChar = expression[i];
+      if (currChar >= '0' && currChar <= '9') {
+        currentNumber += currChar;
+      } else {
+        numbers.push(Number(currentNumber));
+        operators.push(currChar);
+        currentNumber = '';
+      }
+    }
+    // The last number in the expression, e.g. '15+5', is pushed. 
+    // When loop ends, '5' is still sitting in 'currentNumber' since no operator after it
+    // to trigger push during loop. Last number is now pushed. 
+    numbers.push(Number(currentNumber));
   }
