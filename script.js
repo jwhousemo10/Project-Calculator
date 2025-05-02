961216c443e3ccf8df5c0144df5d72af7f9cc36d
Joakim Husmo <joakimhusmo@outlook.com>
2025-05-02 11:08:24 +0200

    Add Button function to script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let expression = '';
 
 // Stores a reference to the display window in a variable.
 const display = document.getElementById('displayWindow');
+
+// Function called whenever a button is clicked - both number and operator.
+  // Adds value of button to expression string, and updates display window.
+  // Note: The function is called in the HTML file's onclick attribute. 
+  function addToExpression(value) {
+    expression += value;
+    updateDisplay(expression);
+  }
