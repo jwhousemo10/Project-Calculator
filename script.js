0c51bfef0cde880c0accae7a1b429e9c328098bd
Joakim Husmo <joakimhusmo@outlook.com>
2025-05-02 10:40:27 +0200

    Add Display window ref to script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 // User's input kept as a string - initial value an empty string. 
 let expression = '';
 
+// Stores a reference to the display window in a variable.
+const display = document.getElementById('displayWindow');
